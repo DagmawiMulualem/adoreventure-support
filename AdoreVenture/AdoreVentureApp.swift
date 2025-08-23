528ae78ecdbfdbe29c6d3781b51117a954e2bfb1
Dagmawi Mulualem <Dagmawi.m.mulualem@gmail.com>
2025-08-23 02:20:36 -0400

    Initial Commit

---
+++ AdoreVenture/AdoreVentureApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdoreVentureApp.swift
+//  AdoreVenture
+//
+//  Created by Dagmawi Mulualem on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct AdoreVentureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdoreVentureTests/AdoreVentureTests.swift
@@ -0,0 +1,17 @@
+//
+//  AdoreVentureTests.swift
+//  AdoreVentureTests
+//
+//  Created by Dagmawi Mulualem on 8/23/25.
+//
+
+import Testing
+@testable import AdoreVenture
+
+struct AdoreVentureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
